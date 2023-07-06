38e0dfa384787553aa9bc0d16d697d91e61b9ffc
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-06 11:13:21 -0400

    adds functionality to take command line arguments as search query

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,20 @@
 const request = require("request");
 
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', (err, response, body) => {
+const input = process.argv;
+if (input.length < 3) throw new Error("ERROR: Not enough args");
+if (input.length > 3) throw new Error("ERROR: Too many args");
+
+const queryParam = input[2].toLowerCase();
+const baseURL = 'https://api.thecatapi.com/v1/breeds/search?q=';
+
+request(baseURL + queryParam, (err, response, body) => {
   if (err) {
     const status = `RESPONSE: ${response.statusCode}`;
     const message = err.message;
     console.log(`${status} ${message}`);
   }
-  // const status = `RESPONSE: ${response.statusCode}\n`;
-  const result = JSON.parse(body)[0].description;
+  const result = `\n${JSON.parse(body)[0].description}\n`;
 
   console.log(result);
 });
+
