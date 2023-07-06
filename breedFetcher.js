eb0b996c967e879bfd0d5b3257b24bcfaf26fced
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-06 11:01:18 -0400

    reduces output to breed description

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,9 +2,12 @@ const request = require("request");
 
 request('https://api.thecatapi.com/v1/breeds/search?q=sib', (err, response, body) => {
   if (err) {
-    console.log(err.message);
+    const status = `RESPONSE: ${response.statusCode}`;
+    const message = err.message;
+    console.log(`${status} ${message}`);
   }
-  const status = `RESPONSE: ${response.statusCode}\n`;
-  const result = JSON.parse(body);
-  console.log(status, result);
+  // const status = `RESPONSE: ${response.statusCode}\n`;
+  const result = JSON.parse(body)[0].description;
+
+  console.log(result);
 });
